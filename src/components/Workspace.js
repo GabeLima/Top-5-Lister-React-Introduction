48b3de026f3254a9fd1f04ebab25ddc3bd2e32e2
“Gabriello <gabriello.lima@stonybrook.edu>
2021-09-24 15:42:13 -0400

    Item editing works, just need to figure out a callback to save it in APP.js.

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -28,7 +28,20 @@ export default class Workspace extends React.Component {
           }));
     }
     handleUpdate = (event) => {
-        this.setState({ text: event.target.value });
+        let items = this.state.items;
+        items[this.state.itemNumber] = event.target.value;
+        this.setState({ text: event.target.value, items: items});
+        //GOTTA SAVE IT
+    }
+    handleBlur = (event) => {
+        this.handleUpdate(event);
+        this.setState({ itemNumber: -1});
+    }
+    handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            this.handleUpdate(event);
+            this.setState({ itemNumber: -1});
+        }
     }
     render() {
         const { currentList } = this.props;
@@ -36,63 +49,40 @@ export default class Workspace extends React.Component {
         if (currentList) {
             items = currentList.items;
         }
-        if (this.state.editActive) {
-            //{...this.state.itemNumber === 0? <input/>: <div className="top5-item" onClick={(e) => {this.handleClick(e, 0, items);}}>{items[0]}</div>}
-            return (
-                <div id="top5-workspace">
-                    <div id="workspace-edit">
-                        <div id="edit-numbering">
-                            <div className="item-number">1.</div>
-                            <div className="item-number">2.</div>
-                            <div className="item-number">3.</div>
-                            <div className="item-number">4.</div>
-                            <div className="item-number">5.</div>
-                        </div>
-                        <div id="edit-items">
-                            {this.state.itemNumber === 0? 
-                            <input className="top5-item" onChange={this.handleUpdate} defaultValue={this.state.text} type='text'/>: 
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 0, items);}}>{items[0]}</div>}
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 1, items);}}>{items[1]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 2, items);}}>{items[2]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 3, items);}}>{items[3]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 4, items);}}>{items[4]}</div>
-                        </div>
+        return (
+            <div id="top5-workspace">
+                <div id="workspace-edit">
+                    <div id="edit-numbering">
+                        <div className="item-number">1.</div>
+                        <div className="item-number">2.</div>
+                        <div className="item-number">3.</div>
+                        <div className="item-number">4.</div>
+                        <div className="item-number">5.</div>
+                    </div>
+                    <div id="edit-items">
+                    {this.state.itemNumber === 0? 
+                        <input className="top5-item" autoFocus onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} onChange={this.handleUpdate} defaultValue={items[0]} type='text'/>: 
+                        <div className="top5-item"  onClick={(e) => {this.handleClick(e, 0, items); }}>{items[0]}</div>}
+
+
+                        {this.state.itemNumber === 1? 
+                        <input className="top5-item" autoFocus onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} onChange={this.handleUpdate} defaultValue={items[1]} type='text'/>: 
+                        <div className="top5-item"  onClick={(e) => {this.handleClick(e, 1, items); }}>{items[1]}</div>}
+
+                        {this.state.itemNumber === 2? 
+                        <input className="top5-item" autoFocus onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} onChange={this.handleUpdate} defaultValue={items[2]} type='text'/>: 
+                        <div className="top5-item"  onClick={(e) => {this.handleClick(e, 2, items); }}>{items[2]}</div>}
+
+                        {this.state.itemNumber === 3? 
+                        <input className="top5-item" autoFocus onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} onChange={this.handleUpdate} defaultValue={items[3]} type='text'/>: 
+                        <div className="top5-item"  onClick={(e) => {this.handleClick(e, 3, items); }}>{items[3]}</div>}
+                        
+                        {this.state.itemNumber === 4? 
+                        <input className="top5-item" autoFocus onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} onChange={this.handleUpdate} defaultValue={items[4]} type='text'/>: 
+                        <div className="top5-item"  onClick={(e) => {this.handleClick(e, 4, items); }}>{items[4]}</div>}
                     </div>
                 </div>
-            )
-            // return (
-            //     <input
-                
-            //         // id={"edit-items"} //+ this.state.itemNumber
-            //         // className='top5-item'
-            //         type='text'
-            //         // onKeyPress={this.handleKeyPress}
-            //         // onBlur={this.handleBlur}
-            //         onChange={this.handleUpdate}
-            //         defaultValue={this.state.text}
-            //     />)
-        }
-        else{
-            return (
-                <div id="top5-workspace">
-                    <div id="workspace-edit">
-                        <div id="edit-numbering">
-                            <div className="item-number">1.</div>
-                            <div className="item-number">2.</div>
-                            <div className="item-number">3.</div>
-                            <div className="item-number">4.</div>
-                            <div className="item-number">5.</div>
-                        </div>
-                        <div id="edit-items">
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 0, items);}}>{items[0]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 1, items);}}>{items[1]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 2, items);}}>{items[2]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 3, items);}}>{items[3]}</div>
-                            <div className="top5-item" onClick={(e) => {this.handleClick(e, 4, items);}}>{items[4]}</div>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
+            </div>
+        )
     }
 }
